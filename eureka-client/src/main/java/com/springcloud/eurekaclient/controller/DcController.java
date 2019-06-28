520ddbf2330669342944d4881226bffe6390ada4
root <345433326@qq.com>
2019-06-28 14:44:28 +0800

    feign

--- eureka-client/src/main/java/com/springcloud/eurekaclient/controller/DcController.java
+++ eureka-client/src/main/java/com/springcloud/eurekaclient/controller/DcController.java
@@ -1,35 +1,37 @@
 package com.springcloud.eurekaclient.controller;
 
-import com.springcloud.eurekaclient.pojo.User;
+import com.springcloud.eurekaclientapi.User;
+import com.springcloud.eurekaclientapi.HelloClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DcController {
+public class DcController implements HelloClient {
     @Autowired
     DiscoveryClient discoveryClient;
 
-    @GetMapping("/dc")
-    public String dc() throws InterruptedException {
-        //Hystrix 默认超时时间是2000毫秒，可以触发断路器
-        //Thread.sleep(5000L);
+    @Override
+    public String consumer() {
         String services = "services:"+discoveryClient.getServices();
         System.out.println(services);
         return services;
+
     }
 
-    @GetMapping("/hello1")
-    public String hello(@RequestParam String name){
+    @Override
+    public String hello(String name) {
         return "RequestParam: hello "+name;
     }
 
-    @GetMapping("/hello2")
-    public User hello(@RequestHeader String name,@RequestHeader Integer age){
+    @Override
+    public User hello(String name, Integer age) {
         return new User(name,age);
     }
-    @PostMapping("/hello3")
-    public String hello(@RequestBody User user){
+
+    @Override
+    public String hello(User user) {
         return "RequestBody: hello " + user.getName()+","+user.getAge();
     }
+
 }
